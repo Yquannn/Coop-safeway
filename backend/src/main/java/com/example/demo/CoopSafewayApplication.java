6277109fee2ebbc8728c2845712e816c39177f33
Earl Eustacio <103189997+EEarlll@users.noreply.github.com>
2026-01-30 21:06:01 +0800

    Commit : add backend

---
+++ backend/src/main/java/com/example/demo/CoopSafewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopSafewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopSafewayApplication.class, args);
+	}
+
+}
